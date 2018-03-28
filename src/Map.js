ff28d206d4924927aaefcdeb5dbf9fe159e90dec
Ryan <zipzop@Ryans-MBP.fios-router.home>
2018-03-27 21:04:35 -0400

    Custom Icons

--- src/Map.js
+++ src/Map.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Map, TileLayer, GeoJSON } from 'react-leaflet'
 import './App.css';
-import {testJson} from './places'
+import { testJson } from './places'
+import L from 'leaflet'
 
 let geojson = testJson();
 
@@ -18,14 +19,17 @@ class MapOne extends Component {
                 center={this.state.center}
                 zoom={8}
             >
+
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     maxZoom={18}
+
                 />
 
                 <GeoJSON
                     data={geojson}
                     onEachFeature={onEachFeature}
+                    pointToLayer={pointToLayer}
                 />
             </Map>
         );
@@ -35,8 +39,30 @@ export default MapOne;
 
 const onEachFeature = (feature, layer) => {
     const popUpContent = [];
-    for (const prop in feature.properties){
-        popUpContent.push(prop + ': ' + feature.properties[prop])
+    for (const prop in feature.properties) {
+        if (feature.properties[prop] != null) {
+            popUpContent.push(prop + ': ' + feature.properties[prop])
+        }
     }
     layer.bindPopup(popUpContent.join('<br />'))
 }
+
+
+const catholicCross = L.icon({
+    iconUrl: require('leaflet/dist/images/GenericCross.svg'),
+    iconSize: [38, 95], 
+});
+
+const russianOrthodoxIcon = L.icon({
+    iconUrl: require('leaflet/dist/images/RussianOrthodox.svg'),
+    iconSize: [38, 95], 
+});
+
+const pointToLayer = (feature, latlng) => {
+    if(feature.properties['place-details-order'] === 'Basilian'){
+    return L.marker(latlng, {icon:russianOrthodoxIcon}); 
+    }
+    else{
+    return L.marker(latlng, {icon:catholicCross}); 
+    }
+ }
