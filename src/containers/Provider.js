c66b557926762f11c386d1dea81c09d33809ba17
RyanMarshall5765 <RyanMarshall5765@gmail.com>
2018-06-08 19:02:20 -0400

    Fixes search results returning Undefined (Issue: Names are on same line)

--- src/containers/Provider.js
+++ src/containers/Provider.js
@@ -1,12 +1,34 @@
 import { geojson } from '../data/places.js'
 
+
+
+const nameParse = (data) => {
+  return Object.values(data).map((value) => {
+      if (value) {
+          if (typeof value === "object") {
+              return nameParse(value);
+          } else {
+              return (value)
+          }
+      } else {
+          return ('Here')
+      }
+  })
+};
+const nameContent = geojson.features.map((feature) => {
+  const name = feature.properties.place.names[1];
+  return name ? nameParse(name) : null;
+});
+
+console.log(nameContent)
+
 class Provider {
   async search({ query }) {
     return geojson.features.map(function (feature) {
       return {
         x: feature.geometry.coordinates[0],
         y: feature.geometry.coordinates[1],
-        label: feature.properties.place.names.name,
+        label: nameContent,
         bounds: [
           [Number, Number], 
           [Number, Number]
