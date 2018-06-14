dcca15e10011ee7fa1b3e14eb5112a4f06e479d1
RyanMarshall5765 <RyanMarshall5765@gmail.com>
2018-06-14 13:25:31 -0400

    Adds airbnb formatting

--- src/components/PlacesMap.js
+++ src/components/PlacesMap.js
@@ -1,110 +1,112 @@
-import React, { Component } from 'react';
-import { Map, TileLayer, GeoJSON } from 'react-leaflet'
-import L from 'leaflet'
-import { geojson } from '../data/places.js'
-import SearchBar from './SearchBar.js'
-import MarkerClusterGroup from 'react-leaflet-markercluster';
-import '../containers/App.css';
-import 'leaflet.markercluster/dist/MarkerCluster.css';
-import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
+import React, { Component } from "react";
+import { Map, TileLayer, GeoJSON } from "react-leaflet";
+import L from "leaflet";
+import { geojson } from "../data/places.js";
+import SearchBar from "./SearchBar.js";
+import MarkerClusterGroup from "react-leaflet-markercluster";
+import "../containers/App.css";
+import "leaflet.markercluster/dist/MarkerCluster.css";
+import "leaflet.markercluster/dist/MarkerCluster.Default.css";
 
 export class PlacesMap extends Component {
-    constructor(props) {
-        super(props)
-        this.onEachFeature = this.onEachFeature.bind(this)
-        this.pointToLayer = this.pointToLayer.bind(this)
-        this.parseGeoJson = this.parseGeoJson.bind(this)
-        this.state = {
-            center: [37.73, 14.20],
-            orthodoxCross: L.icon({
-                iconUrl: 'images/orthodox_cross_black.svg',
-                iconSize: [38, 95],
-            }),
-            simpleCrossBlue: L.icon({
-                iconUrl: 'images/simple_cross_blue.svg',
-                iconSize: [38, 95],
-            }),
-            simpleCrossBlack: L.icon({
-                iconUrl: 'images/simple_cross_black.svg',
-                iconSize: [38, 95],
-            })
-        }
-    }
-
-    parseGeoJson = (data) => {
-        return Object.entries(data).map(([key, value]) => {
-            if (value) {
-                if (typeof value === "object") {
-                    return this.parseGeoJson(value);
-                } else {
-                    return (`${key} : ${value} <br>`)
-                }
-            } else {
-                return (`${key} is empty <br>`)
-            }
-        })
+  constructor(props) {
+    super(props);
+    this.onEachFeature = this.onEachFeature.bind(this);
+    this.pointToLayer = this.pointToLayer.bind(this);
+    this.parseGeoJson = this.parseGeoJson.bind(this);
+    this.state = {
+      center: [37.73, 14.2],
+      zoom: 8,
+      maxZoom: 18,
+      orthodoxCross: L.icon({
+        iconUrl: "images/orthodox_cross_black.svg",
+        iconSize: [38, 95]
+      }),
+      simpleCrossBlue: L.icon({
+        iconUrl: "images/simple_cross_blue.svg",
+        iconSize: [38, 95]
+      }),
+      simpleCrossBlack: L.icon({
+        iconUrl: "images/simple_cross_black.svg",
+        iconSize: [38, 95]
+      })
     };
+  }
 
-    onEachFeature = (feature, layer) => {
+  parseGeoJson(data) {
+    return Object.entries(data).map(([key, value]) => {
+      if (value) {
+        if (typeof value === "object") {
+          return this.parseGeoJson(value);
+        } else {
+          return `${key} : ${value} <br>`;
+        }
+      } else {
+        return `${key} is empty <br>`;
+      }
+    });
+  }
 
-        const placeContent = geojson.features.map((feature) => {
-            const place = feature.properties.place;
-            return place ? this.parseGeoJson(place) : null;
-        })
+  placeContent(feature) {
+    const place = feature.properties.place;
+    return place ? this.parseGeoJson(place) : null;
+  }
 
-        const locationContent = geojson.features.map((feature) => {
-            const location = feature.properties.location;
-            return location ? this.parseGeoJson(location) : null;
-        })
+  locationContent(feature) {
+    const location = feature.properties.location;
+    return location ? this.parseGeoJson(location) : null;
+  }
 
-        const content = `<div class="tabs">
+  onEachFeature(feature, layer) {
+    const content = `<div class="tabs">
             <div class="tab" id="places_tab">
             <div class="content">
-            ${placeContent} 
+            ${this.placeContent(feature)} 
             </div>
             </div> 
             <div class="tab" id="location_tab">
             <div class="content">
-            ${locationContent} 
+            ${this.locationContent(feature)} 
             </div>
             </div>
             <ul class="tabs-link">
             <li class="tab-link"> <a href="#places_tab"><span>Places</span></a></li>
             <li class="tab-link"> <a href="#location_tab"><span>Location</span></a></li>
             </ul>
-            </div>`
+            </div>`;
 
+    layer.bindPopup(content);
+  }
 
-        layer.bindPopup(content)
+  pointToLayer = (feature, latlng) => {
+    switch (feature.properties.place.details.order) {
+      case "Basilian":
+        return L.marker(latlng, { icon: this.state.orthodoxCross });
+      case "Augustinian Canons":
+        return L.marker(latlng, { icon: this.state.simpleCrossBlue });
+      default:
+        return L.marker(latlng, { icon: this.state.simpleCrossBlack });
     }
+  };
 
-    pointToLayer = (feature, latlng) => {
-        switch (feature.properties.place.details.order) {
-            case 'Basilian':
-                return L.marker(latlng, { icon: this.state.orthodoxCross });
-            case 'Augustinian Canons':
-                return L.marker(latlng, { icon: this.state.simpleCrossBlue });
-            default:
-                return L.marker(latlng, { icon: this.state.simpleCrossBlack });
-        }
-    }
-
-
-    render() {
-        return (
-            <div>
-                <Map center={this.state.center} zoom={8}>
-                    <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" maxZoom={18} />
-                    <SearchBar />
-                    <MarkerClusterGroup>
-                        <GeoJSON
-                            data={geojson}
-                            onEachFeature={this.onEachFeature}
-                            pointToLayer={this.pointToLayer} />
-                    </MarkerClusterGroup>
-                </Map>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Map center={this.state.center} zoom={this.state.zoom}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            maxZoom={this.state.maxZoom}
+          />
+          <SearchBar />
+          <MarkerClusterGroup>
+            <GeoJSON
+              data={geojson}
+              onEachFeature={this.onEachFeature}
+              pointToLayer={this.pointToLayer}
+            />
+          </MarkerClusterGroup>
+        </Map>
+      </div>
+    );
+  }
 }
-
--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -1,53 +1,51 @@
-import React, { Component } from 'react';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
-import { PlacesMap } from '../components/PlacesMap'
-
-
+import React, { Component } from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import { PlacesMap } from "../components/PlacesMap";
 
 class NavBar extends Component {
-    render() {
-        return (
-            <div>
-                <Tabs>
-                    <TabList>
-                        <Tab>Places</Tab>
-                        <Tab>People</Tab>
-                        <Tab>Home</Tab>
-                        <Tab>Chattels</Tab>
-                        <Tab>Essays</Tab>
-                        <Tab>Analytics</Tab>
-                        <Tab>Resources</Tab>
-                        <Tab>About</Tab>
-                    </TabList>
-                    <TabPanel>
-                        <h2>Places</h2>
-                        <PlacesMap />
-                    </TabPanel>
-                    <TabPanel>
-                        <h2>People</h2>
-                    </TabPanel>
-                    <TabPanel>
-                        <h2>Home</h2>
-                    </TabPanel>
-                    <TabPanel>
-                        <h2>Chattels</h2>
-                    </TabPanel>
-                    <TabPanel>
-                        <h2>Essays</h2>
-                    </TabPanel>
-                    <TabPanel>
-                        <h2>Analytics</h2>
-                    </TabPanel>
-                    <TabPanel>
-                        <h2>Resources</h2>
-                    </TabPanel>
-                    <TabPanel>
-                        <h2>About</h2>
-                    </TabPanel>
-                </Tabs>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Tabs>
+          <TabList>
+            <Tab>Places</Tab>
+            <Tab>People</Tab>
+            <Tab>Home</Tab>
+            <Tab>Chattels</Tab>
+            <Tab>Essays</Tab>
+            <Tab>Analytics</Tab>
+            <Tab>Resources</Tab>
+            <Tab>About</Tab>
+          </TabList>
+          <TabPanel>
+            <h2>Places</h2>
+            <PlacesMap />
+          </TabPanel>
+          <TabPanel>
+            <h2>People</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Home</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Chattels</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Essays</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Analytics</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Resources</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>About</h2>
+          </TabPanel>
+        </Tabs>
+      </div>
+    );
+  }
 }
 export default NavBar;
