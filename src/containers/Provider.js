cc2e9792d9e10177861e4c88e6415537d3bc656e
RyanMarshall5765 <RyanMarshall5765@gmail.com>
2018-06-14 20:54:23 -0400

    Adds fully functional search bar

--- src/containers/Provider.js
+++ src/containers/Provider.js
@@ -1,33 +1,18 @@
 import { geojson } from "../data/places.js";
 
 class Provider {
-  parse(data) {
-    return Object.values(data).map(value => {
-      if (value) {
-        return typeof value === "object" ? this.parse(value) : value;
-      }
-      return null;
-    });
-  }
-
-  nameContent(feature) {
-    const name = feature.properties.place.names[0];
-    return name ? this.parse(name) : null;
-  }
-
   async search({ query }) {
     return geojson.features
       .filter(
         feature =>
-          this.nameContent(feature)
-            .toString()
+        feature.properties.place.names[0].name
             .toLowerCase()
             .indexOf(query.toLowerCase()) > -1
       )
       .map(feature => ({
         x: feature.geometry.coordinates[0],
         y: feature.geometry.coordinates[1],
-        label: this.nameContent(feature)
+        label: feature.properties.place.names[0].name
       }));
   }
 }
