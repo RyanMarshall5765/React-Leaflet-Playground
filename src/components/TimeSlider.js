d08def16c828eb749f14f268cf31116786970dcb
RyanMarshall5765 <RyanMarshall5765@gmail.com>
2018-06-26 15:04:02 -0400

    Updates TimeSlider styling / thought process

--- src/components/TimeSlider.js
+++ src/components/TimeSlider.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import Slider from "rc-slider";
-// import { geojson } from "../data/places";
+import { geojson } from "../data/places";
 import "rc-slider/assets/index.css";
+// import { GeoJSON } from "react-leaflet";
 
-const style = { width: 400, margin: 50 };
 const createSliderWithTooltip = Slider.createSliderWithTooltip;
 const Range = createSliderWithTooltip(Slider.Range);
 
@@ -14,42 +14,50 @@ class TimeSlider extends Component {
       lowerBound: 1100,
       upperBound: 1250,
       value: [1100, 1250],
-      maxValue: 2018
+      maxValue: 2018,
+      style: { width: 400, margin: 50 }
     };
   }
+
   log(value) {
     console.log(value);
   }
+
   onLowerBoundChange = e => {
     this.setState({ lowerBound: +e.target.value });
   };
   onUpperBoundChange = e => {
     this.setState({ upperBound: +e.target.value });
   };
+
   onSliderChange = value => {
     this.log(value);
     this.setState({
       value
     });
   };
+
   handleApply = () => {
     const { lowerBound, upperBound } = this.state;
     this.setState({ value: [lowerBound, upperBound] });
   };
-  // dynamicallyChangeMarkers(feature, lowerBound, upperBound) {
-  //   return feature.properties.map(time => {
-  //     if (
-  //       feature.properties.details["earliest-attestation"] >
-  //         lowerBoundSliderValue &&
-  //       feature.properties.details["earliest-attestation"] <
-  //         upperBoundSliderValue
-  //     ) {
-  //       return <GeoJSON With corresponding data />;
-  //     } else {
-  //       return nothing;
-  //     }
-  //   });
-  // }
+
+  compareDate() {
+    return geojson.features.map(feature => {
+      if (
+        feature.properties.place.details["earliest-attestation"] >=
+          this.state.lowerBound &&
+        feature.properties.place.details["earliest-attestation"] <=
+          this.state.upperBound
+      ) {
+        return console.log("Working");
+      } else {
+        return null;
+      }
+    });
+  }
+  // Add filter function to remove data that does not meet the criteria to be displayed.
+
   render() {
     return (
       <div>
@@ -74,7 +82,7 @@ class TimeSlider extends Component {
           allowCross={false}
           value={this.state.value}
           onChange={this.onSliderChange}
-          style={style}
+          style={this.state.style}
           max={this.state.maxValue}
           tipFormatter={value => `${value}`}
         />
