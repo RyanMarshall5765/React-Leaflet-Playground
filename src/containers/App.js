590ff58c69f09263bc641e1bfbeb85fb99d7dfba
RyanMarshall5765 <RyanMarshall5765@gmail.com>
2018-07-11 15:02:29 -0400

    Outlines initla app layout

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,20 @@
-import React, { Component } from 'react';
-import NavBar from './NavBar';
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import NavBar from "./NavBar";
+import store from "../store"
 
-// App component
 class App extends Component {
   render() {
-    return <NavBar />;
+    return (
+      <Provider store={store}>
+        <div>
+          <header>
+            <h1> Banner Here </h1>
+          </header>
+          <NavBar />
+        </div>
+      </Provider>
+    );
   }
 }
 
