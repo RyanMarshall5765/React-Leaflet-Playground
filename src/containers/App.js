94ba3317d3248e91ce1f9851c5914fe81a52893a
RyanMarshall5765 <RyanMarshall5765@gmail.com>
2018-07-12 13:50:28 -0400

    Adds redux store and removes temp banner

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
 import NavBar from "./NavBar";
-import store from "../store"
+import store from "../store";
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <div>
-          <header>
-            <h1> Banner Here </h1>
-          </header>
           <NavBar />
         </div>
       </Provider>
