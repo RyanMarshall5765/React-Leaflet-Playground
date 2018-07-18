d2136e381ba9adb6b4ed737b8cf6efb84ff4a654
RyanMarshall5765 <RyanMarshall5765@gmail.com>
2018-07-18 12:45:30 -0400

    Removes react-redux Provider (moved to src/index.js)

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from "react";
-import { Provider } from "react-redux";
 import NavBar from "./NavBar";
-import store from "../store";
 
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <div>
-          <NavBar />
-        </div>
-      </Provider>
+      <div>
+        <NavBar />
+      </div>
     );
   }
 }
