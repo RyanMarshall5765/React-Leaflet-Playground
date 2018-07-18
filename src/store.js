4b0cd1f5bad92f088357e05ab5d6446360befd54
RyanMarshall5765 <RyanMarshall5765@gmail.com>
2018-07-18 12:01:20 -0400

    Adds react-router for redux

--- src/store.js
+++ src/store.js
@@ -1,16 +1,31 @@
 import { createStore, applyMiddleware, compose } from "redux";
+import { connectRouter, routerMiddleware } from "connected-react-router";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
-const initalState = {};
-const middleware = [thunk];
+export const history = createHistory();
+
+const initialState = {};
+const enhancers = [];
+const middleware = [thunk, routerMiddleware(history)];
+
+if (process.env.NODE_ENV === "development") {
+  const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
+
+  if (typeof devToolsExtension === "function") {
+    enhancers.push(devToolsExtension());
+  }
+}
+
+const composedEnhancers = compose(
+  applyMiddleware(...middleware),
+  ...enhancers
+);
+
 const store = createStore(
-  rootReducer,
-  initalState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  connectRouter(history)(rootReducer),
+  initialState,
+  composedEnhancers
 );
 
 export default store;
