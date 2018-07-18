e8347ff44b591f8ace76f5ba6ad9cc36a9192ff9
RyanMarshall5765 <RyanMarshall5765@gmail.com>
2018-07-18 12:00:56 -0400

    Removes wrong implementation

--- src/actions/mapActions.js
+++ src/actions/mapActions.js
@@ -1,17 +1,5 @@
 import {} from "./types";
 
-export const fetchGeojson = data => dispatch => {
-// Current way to pull data
-//   return Object.entries(data).map(([key, value]) => {
-//     if (value) {
-//       return typeof value === "object"
-//         ? this.parseGeoJson(value)
-//         : `${key} : ${value} <br>`;
-//     }
-//     return `${key} is empty <br>`;
-//   });
-};
-
 // example fetch function
 // export const fetchData = () => dispatch => {
 //   fetch("API URL")
