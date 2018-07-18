4b5fc0656ec913a7322ec3e8613f6b1ce12b6ee4
RyanMarshall5765 <RyanMarshall5765@gmail.com>
2018-07-18 15:23:32 -0400

    Removes NavBar and setups up routes for home and places

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
-import NavBar from "./NavBar";
+import { Route, Link } from "react-router-dom";
+import Places from "../containers/Places";
+import Home from "../containers/Home";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <NavBar />
+        <header>
+          <Link to="/">Home</Link>
+          <Link to="/Places">Places</Link>
+        </header>
+        <main>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/places" component={Places} />
+        </main>
       </div>
     );
   }
